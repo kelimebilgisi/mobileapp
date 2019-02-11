0d075dc968babe032f4b7599985ffea4824d59f9
Arturs Eimanis <arturseimanis@gmail.com>
2019-02-11 14:17:42 +0200

    🔥 Remove unnecessary changes

--- Toggl.Daneel/Extensions/Reactive/UITableViewExtensions.cs
+++ Toggl.Daneel/Extensions/Reactive/UITableViewExtensions.cs
@@ -12,8 +12,28 @@ namespace Toggl.Daneel.Extensions.Reactive
 {
     public static class UITableViewExtensions
     {
-        public static IDisposable Bind<TModel, TCell>(this IReactive<UITableView> tableView, ReactiveSectionedListTableViewSource<TModel, TCell> dataSource, IObservable<bool> suggestCreation = null)
+        public static IDisposable Bind<TModel, TCell>(this IReactive<UITableView> tableView, ReactiveSectionedListTableViewSource<TModel, TCell> dataSource)
             where TCell : BaseTableViewCell<TModel>
-            => new ReactiveTableViewBinder<TModel, TCell>(tableView.Base, dataSource, suggestCreation);
+            => new ReactiveTableViewBinder<TModel, TCell>(tableView.Base, dataSource);
+
+        public static IObserver<IEnumerable<CollectionSection<THeader, TModel>>> ReloadSections<THeader, TModel>(
+            this IReactive<UITableView> reactive, BaseTableViewSource<THeader, TModel> dataSource)
+        {
+            return Observer.Create<IEnumerable<CollectionSection<THeader, TModel>>>(list =>
+            {
+                dataSource.SetSections(list);
+                reactive.Base.ReloadData();
+            });
+        }
+
+        public static IObserver<IEnumerable<TModel>> ReloadItems<THeader, TModel>(
+            this IReactive<UITableView> reactive, BaseTableViewSource<THeader, TModel> dataSource)
+        {
+            return Observer.Create<IEnumerable<TModel>>(list =>
+            {
+                dataSource.SetItems(list);
+                reactive.Base.ReloadData();
+            });
+        }
     }
 }
