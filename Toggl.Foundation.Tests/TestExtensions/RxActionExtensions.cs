00a9328b90f5414221a73e120f6cb6dc1b3d3692
William Barbosa <heytherewill@gmail.com>
2019-02-19 07:45:42 -0300

    :construction: Add extension for awaiting RxActions

---
+++ Toggl.Foundation.Tests/TestExtensions/RxActionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Reactive.Testing;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Toggl.Multivac.Extensions;
+
+namespace Toggl.Foundation.Tests.TestExtensions
+{
+    public static class RxActionExtensions
+    {
+        public static async Task Execute(this UIAction action, TestScheduler testScheduler)
+        {
+            var toAwait = action.Execute();
+            testScheduler.Start();
+            await toAwait;
+        }
+
+        public static async Task Execute<T>(this InputAction<T> action, T input, TestScheduler testScheduler)
+        {
+            var toAwait = action.Execute(input);
+            testScheduler.Start();
+            await toAwait;
+        }
+    }
+}
