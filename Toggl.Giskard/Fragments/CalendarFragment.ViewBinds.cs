bca8e11078200b29ea6cdc58ed2cf34bc9cf1c9a
daividssilverio <daivid.silverio@gmail.com>
2019-02-20 19:32:35 -0400

    :construction: Add the calendar recycler view to the calendar fragemnt

--- Toggl.Giskard/Fragments/CalendarFragment.ViewBinds.cs
+++ Toggl.Giskard/Fragments/CalendarFragment.ViewBinds.cs
@@ -1,12 +1,15 @@
 using Android.Views;
+using Toggl.Giskard.Views;
 
 namespace Toggl.Giskard.Fragments
 {
     public partial class CalendarFragment
     {
+        private CalendarRecyclerView calendarRecyclerView;
+
         protected override void InitializeViews(View view)
         {
-            
+            calendarRecyclerView = view.FindViewById<CalendarRecyclerView>(Resource.Id.calendarRecyclerView);
         }
     }
 }
--- Toggl.Giskard/Fragments/CalendarFragment.cs
+++ Toggl.Giskard/Fragments/CalendarFragment.cs
@@ -1,15 +1,23 @@
 using Android.OS;
 using Android.Views;
 using Toggl.Foundation.MvvmCross.ViewModels.Calendar;
+using Toggl.Giskard.Adapters.Calendar;
+using Toggl.Giskard.Views.Calendar;
 
 namespace Toggl.Giskard.Fragments
 {
     public partial class CalendarFragment : ReactiveFragment<CalendarViewModel>
     {
+        private CalendarLayoutManager calendarLayoutManager;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.CalendarFragment, container, false);
             InitializeViews(view);
+
+            calendarLayoutManager = new CalendarLayoutManager();
+            calendarRecyclerView.SetLayoutManager(calendarLayoutManager);
+            calendarRecyclerView.SetAdapter(new CalendarAdapter(view.Context));
 
             return view;
         }
