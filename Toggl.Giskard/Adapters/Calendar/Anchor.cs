6b54ed1f7093e809cace3e3b313ea289fc467d0d
daividssilverio <daivid.silverio@gmail.com>
2019-02-22 13:18:20 -0400

    :children_crossing: Add testing data to help vizualize how the lm works

--- Toggl.Giskard/Adapters/Calendar/Anchor.cs
+++ Toggl.Giskard/Adapters/Calendar/Anchor.cs
@@ -5,9 +5,16 @@ namespace Toggl.Giskard.Adapters.Calendar
     public class Anchor : Java.Lang.Object
     {
         private readonly int height;
-        private readonly AnchorData[] anchoredData;
+        private AnchorData[] anchoredData;
 
-        public AnchorData[] AnchoredData => anchoredData;
+        public AnchorData[] AnchoredData
+        {
+            get => anchoredData;
+            set
+            {
+                anchoredData = value;
+            }
+        }
 
         public int Height => height;
 
