db57ef034cc180929a5f0b52b232bade2cb1039e
Jordan <jordanteasdale@hotmail.com>
2019-02-27 12:39:06 +0100

    :white_check_mark: Add file for report tests
    Adds a new cs file to contain report tests

---
+++ Toggl.Tests.UI/ReportsTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using static Toggl.Tests.UI.Extensions.MainExtensions;
+using static Toggl.Tests.UI.Extensions.StartTimeEntryExtensions;
+namespace Toggl.Tests.UI
+{
+    public class ReportsTests
+    {
+        public ReportsTests()
+        {
+        }
+    }
+}
